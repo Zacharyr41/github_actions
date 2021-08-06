1c6b1c7ae0d03045f04149c78c46e58b71a1c5bb
BuildTools <unconfigured@null.spigotmc.org>
2021-08-06 09:45:01 -0500

    first commit

---
+++ app/main.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+import ast
+
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"Hello" : "World"}
+
+@app.post("/onesum")
+def sum_plus_one(x : int):
+    return x + 1
+
+@app.post("/listtodict")
+def to_dict_from_tuple(lst : str):
+    lst = ast.literal_eval(lst)
+    if type(lst) != list or len(lst) < 2:
+        return json.dumps({"Error" : "Tuple must be of length two or greater"})
+    else:
+        return {lst[0] : lst[1]}
