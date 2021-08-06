fff05661ac02a9abb51b0d7d8d5927d07ad705d3
BuildTools <unconfigured@null.spigotmc.org>
2021-08-06 11:44:09 -0500

    changed iP in test integration

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,14 +1,14 @@
 import unittest
 import requests
 x = 56
-response = requests.post(f'http://0.0.0.0:8008/onesum?x={x}')
+response = requests.post(f'http://127.0.0.1:5000/onesum?x={x}')
 
 print("RESPONSE: ", response.text)
 print("RESPONSE CODE: ", response.status_code)
 
 assert response.status_code == 200, f"Incorrect Request Format: Invalid Response with code {response.status_code}"
 
-response_two = requests.post('http://0.0.0.0:8008/listtodict', params={'lst' : str([2, 6, 0])})
+response_two = requests.post('http://127.0.0.1:5000/listtodict', params={'lst' : str([2, 6, 0])})
 print("RESPONSE TWO: ", response_two.text)
 print("RESPONSE TWO CODE: ", response_two.status_code)
 
